37e29f219aa8b1d8afd18d32d7f17d0e73c155e0
Skyceer <slavanezox176@gmail.com>
2024-10-25 05:28:28 +0700

    .

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
+from sqlalchemy import Integer, String
+
 from app import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(32), unique=True, nullable=False)
-    link = db.Column(db.String(255), nullable=True)
+    id = db.Column(Integer, primary_key=True)
+    key = db.Column(String, unique=True, nullable=False)
+    link = db.Column(String, nullable=True)
